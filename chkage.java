a57e959f5def36e9738e0a014b3e3ea0fe37d3d6
Sonal Patel <tina_sonal@yahoo.co.in>
2022-07-29 18:19:59 +0100

    chkage

---
+++ chkage.java
@@ -0,0 +1,14 @@
+package IF_hw;
+
+public class chkage {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18)
+        {
+            System.out.println("voting allowed");
+        }
+        else {
+            System.out.println("voting is not allowed");
+        }
+    }
+}
